2a1fd87fd85a3ff1be672d6d15f4d3a64f59ce40
itaseski <itaseski@t.mk>
2023-06-27 19:19:30 +0200

    create views

--- spares/urls.py
+++ spares/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "spares"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:document_id>/", views.detail, name="detail"),
 ]
--- spares/views.py
+++ spares/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from django.http import HttpResponse
+from .models import Document
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the spare index.")
+    document_list = Document.objects.order_by("number")
+    context = {
+        "document_list": document_list,
+    }
+    return render(request, "spares/index.html", context)
 
+
+def detail(request, document_id):
+    document = get_object_or_404(Document, pk=document_id)
+    return render(request, "spares/detail.html", {"document": document})
 
