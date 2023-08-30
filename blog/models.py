a977d64673bcba63e3fa679671b5bf19f37f5f7d
itaseski <itaseski@t.mk>
2023-08-30 13:24:02 +0200

    defining a default sort order

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,7 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish']),]
 
     def __str__(self):
         return self.title
